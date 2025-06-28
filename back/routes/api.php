1b713f9efe0470912038a7c45f19f011822eccb2
YuChunWang-del <2022002171@mails.cust.edu.cn>
2025-06-28 11:17:41 +0800

    course路由

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,84 +1,93 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users\UserController;
+use App\Http\Controllers\Users\PermissionController;
+use App\Http\Controllers\Users\RoleController;
+use App\Http\Controllers\scenario\ScenarioController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ImagesController;
+use App\Http\Controllers\InstancesController;
+use App\Http\Controllers\ContainersController;
 
-    use Illuminate\Support\Facades\Route;
-    use App\Http\Controllers\Users\UserController;
-    use App\Http\Controllers\Users\PermissionController;
-    use App\Http\Controllers\Users\RoleController;
-    use App\Http\Controllers\scenario\ScenarioController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-    /*
-    |--------------------------------------------------------------------------
-    | API Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register API routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | is assigned the "api" middleware group. Enjoy building your API!
-    |
-    */
+Route::prefix('user')->group(function () {
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/id', [UserController::class, 'getUserById'])->middleware('jwtcheck');
+    Route::post('/all', [UserController::class, 'getAllUser'])->middleware('jwtcheck:get-all-users');
+    Route::post('/new', [UserController::class, 'insertNewUser']);
+    Route::post('/update', [UserController::class, 'updateUserInfo'])->middleware('jwtcheck');
+    Route::post('/delete', [UserController::class, 'deleteUser'])->middleware('jwtcheck:edit-users');
+});
 
+Route::prefix('role')->group(function () {
+    Route::post('/all', [RoleController::class, 'getAllRole'])->middleware('jwtcheck:get-all-roles');
+    Route::post('/id', [RoleController::class, 'getRoleById']);
+    Route::post('/new', [RoleController::class, 'newRole'])->middleware('jwtcheck:edit-roles');
+    Route::post('/update', [RoleController::class, 'updateRole'])->middleware('jwtcheck:edit-roles');
+    Route::post('/delete', [RoleController::class, 'deleteRole'])->middleware('jwtcheck:edit-roles');
+    Route::post('/grant', [RoleController::class, 'grantRoles2User'])->middleware('jwtcheck:edit-roles');
+    Route::post('/revoke', [RoleController::class, 'revokeRoleFromUser'])->middleware('jwtcheck:edit-roles');
+});
 
-    Route::prefix("user")->group(function() {
-        Route::post("/login",[UserController::class,"login"]);
-        Route::post("/id",[UserController::class,"getUserById"])->middleware("jwtcheck");
-        Route::post("/all",[UserController::class,"getAllUser"])->middleware("jwtcheck:get-all-users");
-        Route::post("/new",[UserController::class,"insertNewUser"]);
-        Route::post("/update",[UserController::class,"updateUserInfo"])->middleware("jwtcheck");
-        Route::post("/delete",[UserController::class,"deleteUser"])->middleware("jwtcheck:edit-users");
-    });
+Route::prefix('permission')->group(function () {
+    Route::post('/all', [PermissionController::class, 'getAllPermission'])->middleware('jwtcheck:get-all-permissions');
+    Route::post('/id', [PermissionController::class, 'getPermissionById']);
+    Route::post('/new', [PermissionController::class, 'newPermission'])->middleware('jwtcheck:edit-permissions');
+    Route::post('/update', [PermissionController::class, 'updatePermission'])->middleware('jwtcheck:edit-permissions');
+    Route::post('/delete', [PermissionController::class, 'deletePermission'])->middleware('jwtcheck:edit-permissions');
+    Route::post('/grant', [PermissionController::class, 'grantPermission2Role'])->middleware('jwtcheck:edit-permissions');
+    Route::post('/revoke', [PermissionController::class, 'revokePermissionFromRole'])->middleware('jwtcheck:edit-permissions');
+});
 
-    Route::prefix("role")->group(function(){
-        Route::post("/all",[RoleController::class,"getAllRole"])->middleware("jwtcheck:get-all-roles");
-        Route::post("/id",[RoleController::class,"getRoleById"]);
-        Route::post("/new",[RoleController::class,"newRole"])->middleware("jwtcheck:edit-roles");
-        Route::post("/update",[RoleController::class, "updateRole"])->middleware("jwtcheck:edit-roles");
-        Route::post("/delete",[RoleController::class,"deleteRole"])->middleware("jwtcheck:edit-roles");
-        Route::post("/grant", [RoleController::class,"grantRoles2User"])->middleware("jwtcheck:edit-roles");
-        Route::post("/revoke", [RoleController::class,"revokeRoleFromUser"])->middleware("jwtcheck:edit-roles");
-    });
+Route::prefix('scenarios')->group(function () {
+    Route::get('/', [ScenarioController::class, 'index']);
+    Route::post('/', [ScenarioController::class, 'store']);
+    Route::delete('/', [ScenarioController::class, 'destroy']);
+});
 
-    Route::prefix("permission")->group(function() {
-        Route::post("/all",[PermissionController::class,"getAllPermission"])->middleware("jwtcheck:get-all-permissions");
-        Route::post("/id",[PermissionController::class,"getPermissionById"]);
-        Route::post("/new",[PermissionController::class,"newPermission"])->middleware("jwtcheck:edit-permissions");
-        Route::post("/update",[PermissionController::class,"updatePermission"])->middleware("jwtcheck:edit-permissions");
-        Route::post("/delete",[PermissionController::class,"deletePermission"])->middleware("jwtcheck:edit-permissions");
-        Route::post("/grant", [PermissionController::class,"grantPermission2Role"])->middleware("jwtcheck:edit-permissions");
-        Route::post("/revoke", [PermissionController::class,"revokePermissionFromRole"])->middleware("jwtcheck:edit-permissions");
-    });
+Route::prefix('images')->group(function () {
+    Route::get('/', [ImagesController::class, 'index']);
+    Route::post('/', [ImagesController::class, 'store']);
+    Route::put('/', [ImagesController::class, 'update']);
+    Route::delete('/', [ImagesController::class, 'destroy']);
+});
 
+Route::prefix('instances')->group(function () {
+    Route::get('/', [InstancesController::class, 'index']);
+    Route::post('/', [InstancesController::class, 'store']);
+    Route::put('/', [InstancesController::class, 'update']);
+    Route::delete('/', [InstancesController::class, 'destroy']);
+});
 
-    Route::prefix('scenarios')->group(function () {
-        // GET /api/scenarios - 获取所有场景列表
-        Route::get('/', [ScenarioController::class, 'index']);
-        // POST /api/scenarios - 创建一个新场景 (这个您已经有了)
-        Route::post('/', [ScenarioController::class, 'store']);
-        // DELETE /api/scenarios - 删除一个指定场景
-        Route::delete('/', [ScenarioController::class, 'destroy']);
-    });
+Route::prefix('containers')->group(function () {
+    Route::post('/', [ContainersController::class, 'store']);
+    Route::post('/{id}', [ContainersController::class, 'action']);
+    Route::get('/{id}', [ContainersController::class, 'get']);
+    Route::get('/{id}/logs', [ContainersController::class, 'logs']);
+    Route::get('/{id}/inspect', [ContainersController::class, 'inspect']);
+    Route::get('/{id}/binds', [ContainersController::class, 'binds']);
+});
 
-    Route::prefix('images')->group(function () {
-        Route::get('/', [\App\Http\Controllers\ImagesController::class, 'index']);
-        Route::post('/', [\App\Http\Controllers\ImagesController::class, 'store']);
-        Route::put('/', [\App\Http\Controllers\ImagesController::class, 'update']);
-        Route::delete('/', [\App\Http\Controllers\ImagesController::class, 'destroy']);
-    });
+Route::prefix('categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::post('/', [CategoryController::class, 'store'])->middleware('jwtcheck:edit-categories');
+});
 
-    Route::prefix('instances')->group(function () {
-        Route::get('/', [\App\Http\Controllers\InstancesController::class, 'index']);
-        Route::post('/', [\App\Http\Controllers\InstancesController::class, 'store']);
-        Route::put('/', [\App\Http\Controllers\InstancesController::class, 'update']);
-        Route::delete('/', [\App\Http\Controllers\InstancesController::class, 'destroy']);
-    });
-
-    Route::prefix('containers')->group(function () {
-        Route::post('/', [\App\Http\Controllers\ContainersController::class, 'store']);
-        Route::post('/{id}', [\App\Http\Controllers\ContainersController::class, 'action']);
-        Route::get('/{id}', [\App\Http\Controllers\ContainersController::class, 'get']);
-        Route::get('/{id}/logs', [\App\Http\Controllers\ContainersController::class, 'logs']);
-        Route::get('/{id}/inspect', [\App\Http\Controllers\ContainersController::class, 'inspect']);
-        Route::get('/{id}/binds', [\App\Http\Controllers\ContainersController::class, 'binds']);
-    });
-
-?>
+Route::prefix('course-cases')->group(function () {
+    Route::get('/', [CourseController::class, 'index']);
+    Route::post('/', [CourseController::class, 'store'])->middleware('jwtcheck:edit-courses');
+    Route::put('/{id}', [CourseController::class, 'update'])->middleware('jwtcheck:edit-courses');
+    Route::delete('/{id}', [CourseController::class, 'destroy'])->middleware('jwtcheck:edit-courses');
+});
