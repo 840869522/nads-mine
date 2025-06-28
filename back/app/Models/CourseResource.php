d3dcd7cf168b1a4372210bd546122008d8ca5bbe
YuChunWang-del <2022002171@mails.cust.edu.cn>
2025-06-28 10:52:10 +0800

    关系映射

---
+++ back/app/Models/CourseResource.php
@@ -0,0 +1,14 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourseResource extends Model
+{
+    protected $table = 'c_COURSE_RESOURCES';
+    protected $fillable = ['course_id', 'resource_name', 'resource_path', 'type', 'size'];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id','course_id');
+    }
+}
