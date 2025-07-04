f04af30cbc1e3a60343ad6b77f532189be5f5759
李翰 <LF20182003@outlook.com>
2025-07-04 17:08:07 +0800

    fix: fix some error

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -12,14 +12,6 @@ const nextConfig = {
     }
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://10.12.0.102:11180/api/:path*', // 替换为你的后端地址
-      },
-    ];
-  },
 };
 
 export default nextConfig;
