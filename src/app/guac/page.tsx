dc8b90c4fd736853fd7c3ce3c359038378e2f4c8
Pei Yuqing <35334494+840869522@users.noreply.github.com>
2025-07-06 21:06:42 +0800

    fix Guacamole import

--- src/app/guac/page.tsx
+++ src/app/guac/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
 import { useEffect, useRef } from 'react'
-import * as Guacamole from 'guacamole-common-js'
+import Guacamole from 'guacamole-common-js'
 
 export default function GuacPage() {
   const params = useSearchParams();
