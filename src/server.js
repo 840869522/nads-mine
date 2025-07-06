0b8c51d71abc4a37ee4b8ea77400f371b54e8475
840869522 <840869522@qq.com>
2025-07-06 11:44:04 +0800

    3333

--- src/server.js
+++ src/server.js
@@ -85,7 +85,7 @@ app.prepare().then(() => {
   const phpProxy = createProxyMiddleware({
     target: PHP_TARGET_URL,
     changeOrigin: true,
-    pathRewrite: { '^/api/php': '/' },
+    pathRewrite: { '^/api/php': '/api' },
     logLevel: dev ? 'debug' : 'info',
     onError: (err, req, res) => {
       console.error('PHP Proxy error:', err);
