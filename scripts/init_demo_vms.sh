4e32a1699a5d4f40639d72ccdabbf8f8b1359c1f
840869522 <840869522@qq.com>
2025-07-07 00:49:34 +0800

    3333

--- scripts/init_demo_vms.sh
+++ scripts/init_demo_vms.sh
@@ -7,22 +7,22 @@ cd "$DIR"
 # Download minimal Ubuntu image
 if [ ! -f ubuntu-demo.img ]; then
   curl -L -o ubuntu-demo.img \
-    https://cloud-images.ubuntu.com/minimal/releases/22.04/release/ubuntu-22.04-minimal-cloudimg-amd64.img
+    https://cloud-images.ubuntu.com/minimal/releases/jammy/release/ubuntu-22.04-minimal-cloudimg-amd64.img
 fi
 
 # Download Windows evaluation image
-if [ ! -f win-demo.qcow2 ]; then
-  curl -L -o win-demo.qcow2 https://go.microsoft.com/fwlink/?linkid=2215517
-fi
+#if [ ! -f win-demo.qcow2 ]; then
+#  curl -L -o win-demo.qcow2 https://go.microsoft.com/fwlink/?linkid=2215517
+#fi
 
 cd ..
-python3 src/main_cli_local.py create-vm \
+src/.venv/bin/python3 src/main_cli_local.py create-vm \
   --vm-name demo-linux \
   --base-image demo-images/ubuntu-demo.img \
   --admin-password demo123
 
-python3 src/main_cli_local.py create-vm \
-  --vm-name demo-win \
-  --base-image demo-images/win-demo.qcow2 \
-  --admin-password P@ssw0rd
+#python3 src/main_cli_local.py create-vm \
+#  --vm-name demo-win \
+#  --base-image demo-images/win-demo.qcow2 \
+#  --admin-password P@ssw0rd
 
