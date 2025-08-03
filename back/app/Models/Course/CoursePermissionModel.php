bba1200692905f1e441ab4cf68708eb07e56b0e0
YuChunWang-del <2022002171@mails.cust.edu.cn>
2025-08-03 16:17:30 +0800

    权限分配显示管理员用户

--- back/app/Models/Course/CoursePermissionModel.php
+++ back/app/Models/Course/CoursePermissionModel.php
@@ -19,7 +19,6 @@ class CoursePermissionModel
         try {
             $users = DB::table('c_users')
                 ->select('c_username')
-                ->where('c_username', '!=', 'admin') // 排除 c_username = 'admin'
                 ->get()
                 ->toArray();
 
@@ -205,7 +204,7 @@ class CoursePermissionModel
 
             DB::beginTransaction();
             $result = DB::insert(
-                'INSERT INTO ' . self::COURSES_USERS_TABLE . ' (c_username, c_course_id) VALUES (?, ?, NOW(), NOW())',
+                'INSERT INTO ' . self::COURSES_USERS_TABLE . ' (c_username, c_course_id) VALUES (?, ?)',
                 [$userId, $courseId]
             );
 
