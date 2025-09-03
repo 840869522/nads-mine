92001f34b1c775ff015e9198586ec8e47f1faddc
Yic-YU <1395497814@qq.com>
2025-09-03 22:31:42 +0800

    调整新虚拟机脚本，将 flag 文件路径修改为 C:\Windows\System32\flag.txt，并添加 create.bat 的执行命令，同时将 vCPUs 数量从 2 增加到 4。

--- back/app/RunTool/vmscript/newvm_win7.sh
+++ back/app/RunTool/vmscript/newvm_win7.sh
@@ -64,7 +64,8 @@ cat <<EOF > "${INSTANCE_DIR}/user-data"
 hostname: ${VM_NAME}
 runcmd:
   - netsh interface ip set address name="本地连接" static ${IP_ADDRESS} 255.255.0.0 10.100.0.254 1
-  - echo ${FLAG} > C:\flag.txt
+  - echo ${FLAG} > C:\Windows\System32\flag.txt
+  - C:\Windows\System32\create.bat
 EOF
 
 echo "[CONFIG] 正在生成 meta-data..."
@@ -87,7 +88,7 @@ echo "[VIRT] 正在执行 virt-install 命令..."
 virt-install --virt-type kvm \
   --name "${VM_NAME}" \
   --ram=4096 \
-  --vcpus=2 \
+  --vcpus=4 \
   --os-variant win7 \
   --disk path="${DESTINATION_IMAGE_PATH}",device=disk,bus=sata,format=qcow2 \
   --disk path="${INSTANCE_DIR}/config.iso",device=cdrom \
