1920befb18d11b2805c9dcf281a3a667d2eb1e32
李翰 <LF20182003@outlook.com>
2025-09-12 13:13:39 +0800

    add output code

--- langchain/app/controller/llm/chat.py
+++ langchain/app/controller/llm/chat.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 from app.llm.LLMChain import generate_response
 from app.schema.ChatMessage import ChatMessage
 from app.schema.ParseFile import FileParseMessage
 
 from app.llm.ParseFile import parseFile
+
 
 
 chat_route = APIRouter(
@@ -20,5 +21,6 @@ async def chatController(message: ChatMessage):
 @chat_route.post("/parse")
 async def parseFileController(message: FileParseMessage):
     code = await parseFile(message.file_path,message.file_type)
-    
+    return JSONResponse(content={"code": 200, "message": "Success", "data": result})
+
     
