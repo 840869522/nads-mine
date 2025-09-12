a651390c50c1a8d11d2c59d8c43a18ca763e4631
Yic-Yu <1395497814@qq.com>
2025-09-12 22:23:41 +0800

    feat: 添加Windows 10虚拟机创建功能，更新workerman进程ID，新增相关脚本

--- back/app/RunTool/CommandLineService.php
+++ back/app/RunTool/CommandLineService.php
@@ -704,6 +704,32 @@ XML;
         'output' => $process->getOutput()
     ]);
 }
+
+    public function createVmWin10(array $options): void
+{
+    // 指向新的 win10 脚本
+    $scriptPath = app_path('RunTool/vmscript/newvm_win10.sh');
+
+    $args = [
+        $options['id'],
+        $options['image'],
+        $options['switch_name'],
+        $options['vm_name'],
+        $options['image_dir'],
+        $options['instance_base_dir'], 
+    ];
+
+    $command = array_merge([$scriptPath], $args);
+    Log::info('Executing Windows VM creation shell script (win10): ' . implode(' ', $command));
+
+    $process = new Process($command);
+    $process->setTimeout(360); // Windows启动可能较慢，设置更长的超时
+    $process->mustRun(); // 如果失败则抛出异常
+
+    Log::info("Windows VM creation script (win10) for vm '{$options['vm_name']}' executed successfully.", [
+        'output' => $process->getOutput()
+    ]);
+}
 }
 
 // // ```json
