32e60a80e2e8999d8bc00178bfb55a110b3cdfd3
Yic-Yu <1395497814@qq.com>
2025-09-16 23:03:29 +0800

    更新跳过网络模式的镜像列表，新增px4-image1相关镜像

--- back/app/RunTool/CommandLineService.php
+++ back/app/RunTool/CommandLineService.php
@@ -634,7 +634,7 @@ XML;
 
         // d. 设置网络模式为 none，这是后续手动连接的关键
         // 检查镜像名称，如果是特定的数据库镜像则不设置网络模式
-        $skipNetworkImages = ['d_tar_oralcercedb35:v3', 'd_tar_oralcepasswd10:v1','px4-image:latest','px4pro2-image:latest','px4pro-image:latest','mavlink-px4:latest'];
+        $skipNetworkImages = ['d_tar_oralcercedb35:v3', 'd_tar_oralcepasswd10:v1','px4-image:latest','px4pro2-image:latest','px4pro-image:latest','px4-image1:latest','px4-image1:v1'];
         if (!in_array($options['image'], $skipNetworkImages)) {
             $command[] = '--network=none';
         }
