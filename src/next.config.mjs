2d540d3e047d9220b866956045c790a66e0e18b1
李翰 <LF20182003@outlook.com>
2025-09-17 19:52:49 +0800

    fix start.sh

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
     ignoreBuildErrors: true
   },//应确保 ssh2 相关的代码 仅在 Node.js 运行的服务端执行，不要被 Webpack 打包进客户端。
   webpack: (config, { isServer }) => {
-    config.cache = true;
     if (isServer) {
       if (Array.isArray(config.externals)) {
         config.externals.push('ssh2');
