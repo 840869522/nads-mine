752a6f23b4ccb54f5e37f0b5996157511fad396f
赵宇 <zhaoyu@yongtu.io>
2025-09-19 11:53:31 +0800

    feat：环境变量

--- src/constants.ts
+++ src/constants.ts
@@ -280,7 +280,7 @@ export const DEFAULT_NODE_CONFIG: Record<DeviceType, NodeConfig> = {
     deviceName: '容器',
     dockerImage: 'ubuntu:latest',
     // portMappings: '80:80',
-    env: 'ELASTICSEARCH_HOST=10.100.88.88,ELASTICSEARCH_PORT=9200,TZ=Asia/Shanghai'
+    env: 'ELASTICSEARCH_HOST=10.100.88.88,ELASTICSEARCH_PORT=9200,TZ=Asia/Shanghai,ZEEK_ENABLED=1,SYSDIG_ENABLED=1'
   },
   switch: {
     deviceName: '交换机',
