0c39f5aca91847042d4707b788e225aaa091ab93
Yic-YU <1395497814@qq.com>
2025-10-17 15:48:03 +0800

    修复容器路由配置命令，确保以root用户执行

--- back/app/RunTool/CommandLineService.php
+++ back/app/RunTool/CommandLineService.php
@@ -505,7 +505,7 @@ class CommandLineService
         foreach ($containers as $container) {
             $containerName = $container['name'];
             // 使用 ip route replace，若不存在则新增，存在则覆盖，避免重复添加报错
-            $commandReplaceRoute = ['sudo', 'docker', 'exec', $containerName, 'ip', 'route', 'replace', 'default', 'via', $gatewayIpOnly];
+            $commandReplaceRoute = ['sudo', 'docker', 'exec', '--user', 'root', $containerName, 'ip', 'route', 'replace', 'default', 'via', $gatewayIpOnly];
             Log::info("Executing [IP-Config]: Replacing default route for container {$containerName} via {$gatewayIpOnly}");
             (new Process($commandReplaceRoute))->mustRun();
         }
