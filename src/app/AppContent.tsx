f70b58bd21805ac5d86e19f2ba1c0e432840cb56
李翰 <LF20182003@outlook.com>
2025-10-27 18:55:27 +0800

    fix loading page logic

--- src/app/AppContent.tsx
+++ src/app/AppContent.tsx
@@ -11,6 +11,7 @@ import { getCookie } from '@/utils/cookie';
 import { AffixedFabWrapper } from '@/components/layout/AffixedFab';
 import ChatPage from "@/components/chat/page";
 import { userPermissionContext } from '@/contexts/PermissionAndMenuContext';
+import LoadingPage from '@/components/layout/LoadingPage';
 
 const DRAWER_WIDTH = 250;
 
@@ -21,17 +22,22 @@ export default function AppContent({ children }: { children: React.ReactNode }) 
   const router = useRouter();
   const pathname = usePathname();
   const [chatOpen, setChatOpen] = useState<boolean>(false);
+  const [isCheckAuth , setIsAuthCheck] = useState<boolean>(true);
 
   useEffect(() => {
+    console.log("app_context");
     const permissionsData = user?.permission || [];
     const roleData = user?.role || [];
     const token = getCookie("_auth");
     const isGuac = pathname.startsWith('/guac');
     if (!token) {
       if (pathname !== '/login' && !isGuac) {
-        router.replace('/login');
+        setTimeout(() => {
+          setIsAuthCheck(false);
+          router.replace('/login');
+        }, 2000)
       }
-    }else {
+    } else {
       const match = routeAndPermission.find(r => pathname === r.prefix);
       if (match) {
         if (!roleData.includes(UserRole.ADMIN))
@@ -43,11 +49,15 @@ export default function AppContent({ children }: { children: React.ReactNode }) 
     }
   }, [user, pathname, router]);
 
+  if (isCheckAuth) {
+    return <LoadingPage />
+  }
+
   const aiChat = useMemo(() => {
     if (user)
       return (
         <Box>
-          <AffixedFabWrapper onClick={() => { setChatOpen(true)}} />
+          <AffixedFabWrapper onClick={() => { setChatOpen(true) }} />
           {
             chatOpen &&
             <ChatPage open={chatOpen} onClose={() => setChatOpen(false)} width='25vw' />
@@ -57,7 +67,7 @@ export default function AppContent({ children }: { children: React.ReactNode }) 
     else {
       return null;
     }
-  }, [user,chatOpen]);
+  }, [user, chatOpen]);
 
   const showSidebar = Boolean(user) && pathname !== '/login' && !pathname.startsWith('/guac') && !pathname.startsWith('/visualization');
 
@@ -75,7 +85,7 @@ export default function AppContent({ children }: { children: React.ReactNode }) 
           overflow: 'hidden',
         }}
       >
-        {showSidebar ? <PageWrapper sx={{width: chatOpen ? `calc(100% - 25vw)`: "100%"}}>{children}</PageWrapper> : children}
+        {showSidebar ? <PageWrapper sx={{ width: chatOpen ? `calc(100% - 25vw)` : "100%" }}>{children}</PageWrapper> : children}
       </Box>
       {aiChat}
     </Box>
