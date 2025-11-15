392321d043ca44c02f0471e10d325c4682309e32
李翰 <LF20182003@outlook.com>
2025-11-15 16:34:44 +0800

    delete debug info

--- src/logger.js
+++ src/logger.js
@@ -16,7 +16,6 @@ const formatLocalTime = () => {
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
 
-console.log("env",process.env)
 const Logger = winston.createLogger({
     level: process.env.LOG_LEVEL || "info",
     format: winston.format.combine(
